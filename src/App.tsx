b9495bef06db4629e2afc13f61fa2f4d8b1c3624
priteshiroid <pritesh.iroidsolutions@gmail.com>
2025-06-06 09:48:29 +0530

    new changes

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,14 @@ function App() {
                 <a href="https://vite.dev" target="_blank">
                     <img src={viteLogo} className="logo" alt="Vite logo" />
                 </a>
-                <a href="https://react.dev" target="_blank">
+                <a href="https://node.dev" target="_blank">
                     <img
                         src={reactLogo}
-                        className="logo react"
+                        className="logo happy"
                         alt="React logo"
                     />
                 </a>
             </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
         </>
     );
 }
