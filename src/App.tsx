683b38efb11043538e77061ee84eddd893e170b7
priteshiroid <pritesh.iroidsolutions@gmail.com>
2025-06-06 09:46:32 +0530

    merge

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <>
             <div>
@@ -18,11 +16,6 @@ function App() {
                         alt="React logo"
                     />
                 </a>
-            </div>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
             </div>
             <p className="read-the-docs">
                 Click on the Vite and React logos to learn more
